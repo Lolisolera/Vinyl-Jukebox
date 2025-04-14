a47cb48f2225cd8ef481ffbf5d1ed8175e62a5aa
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2025-04-14 12:37:45 +0100

    Update 'SpotifyTrackDTO' file to include a new field to allow storing genre names retrieved from Spotify when fetching a track

--- src/main/java/com/lola/vinyljukebox/dto/SpotifyTrackDTO.java
+++ src/main/java/com/lola/vinyljukebox/dto/SpotifyTrackDTO.java
@@ -3,6 +3,8 @@ package com.lola.vinyljukebox.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class SpotifyTrackDTO {
@@ -10,7 +12,9 @@ public class SpotifyTrackDTO {
     private String name;
     private String previewUrl;
     private String artistName;
+    private List<String> genres; // holds genre names associated with the artist
 }
+
 // This DTO (Data Transfer Object) is used to represent the simplified track information retrieved from the Spotify API,
 // including fields like ID, name, preview URL, and artist name. It's used to map Spotify JSON responses into Java objects
 // that can be handled easily within the application when searching or adding tracks via the Spotify integration.
